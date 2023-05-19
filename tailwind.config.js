d8e002bad21143ebb26149ce6298e7d3d12606e0
ogamxp <xyliganistan100mk@gmail.com>
2023-05-19 17:42:11 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'phone': {max: '600px'},
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
